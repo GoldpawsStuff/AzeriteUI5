d995a3b221f57a287fa8b1ed59bd63c539911be1
Lars Norberg <github@larsnorbergofficial.com>
2022-12-12 10:23:46 +0100

    hide blizzard elements

--- AzeriteUI5/Components/ActionBars/HideBlizzard.lua
+++ AzeriteUI5/Components/ActionBars/HideBlizzard.lua
@@ -24,10 +24,15 @@
 
 --]]
 local Addon, ns = ...
+if (not ns.IsRetail) then
+	return
+end
+
 local ActionBars = ns:GetModule("ActionBars", true)
 if (not ActionBars) then return end
 
-local Blizzard = ActionBars:NewModule("Blizzard", "LibMoreEvents-1.0")
+local Blizzard = ActionBars:NewModule("Blizzard", "LibMoreEvents-1.0", "AceHook-3.0")
+local UIHider = ns.Hider
 
 local purgeKey = function(t, k)
 	t[k] = nil
@@ -58,7 +63,7 @@ local hideActionBarFrame = function(frame, clearEvents)
 		else
 			frame:Hide()
 		end
-		frame:SetParent(ns.Hider)
+		frame:SetParent(UIHider)
 	end
 end
 
@@ -70,35 +75,50 @@ local hideActionButton = function(button)
 	button:SetAttribute("statehidden", true)
 end
 
+Blizzard.NPE_LoadUI = function(self)
+	if not (Tutorials and Tutorials.AddSpellToActionBar) then return end
+
+	-- Action Bar drag tutorials
+	Tutorials.AddSpellToActionBar:Disable()
+	Tutorials.AddClassSpellToActionBar:Disable()
+
+	-- these tutorials rely on finding valid action bar buttons, and error otherwise
+	Tutorials.Intro_CombatTactics:Disable()
+
+	-- enable spell pushing because the drag tutorial is turned off
+	Tutorials.AutoPushSpellWatcher:Complete()
+end
+
 Blizzard.HideBlizzard = function(self)
 
-	--hideActionBarFrame(MainMenuBar, false)
-	--hideActionBarFrame(MultiBarBottomLeft, true)
-	--hideActionBarFrame(MultiBarBottomRight, true)
-	--hideActionBarFrame(MultiBarLeft, true)
-	--hideActionBarFrame(MultiBarRight, true)
-	--hideActionBarFrame(MultiBar5, true)
-	--hideActionBarFrame(MultiBar6, true)
-	--hideActionBarFrame(MultiBar7, true)
+	hideActionBarFrame(MainMenuBar, true)
+	hideActionBarFrame(MultiBarBottomLeft, true)
+	hideActionBarFrame(MultiBarBottomRight, true)
+	hideActionBarFrame(MultiBarLeft, true)
+	hideActionBarFrame(MultiBarRight, true)
+	hideActionBarFrame(MultiBar5, true)
+	hideActionBarFrame(MultiBar6, true)
+	hideActionBarFrame(MultiBar7, true)
 
 	-- Hide MultiBar Buttons, but keep the bars alive
-	--for i=1,12 do
-	--	hideActionButton(_G["ActionButton" .. i])
-	--	hideActionButton(_G["MultiBarBottomLeftButton" .. i])
-	--	hideActionButton(_G["MultiBarBottomRightButton" .. i])
-	--	hideActionButton(_G["MultiBarRightButton" .. i])
-	--	hideActionButton(_G["MultiBarLeftButton" .. i])
-	--	hideActionButton(_G["MultiBar5Button" .. i])
-	--	hideActionButton(_G["MultiBar6Button" .. i])
-	--	hideActionButton(_G["MultiBar7Button" .. i])
-	--end
+	for i=1,12 do
+		hideActionButton(_G["ActionButton" .. i])
+		hideActionButton(_G["MultiBarBottomLeftButton" .. i])
+		hideActionButton(_G["MultiBarBottomRightButton" .. i])
+		hideActionButton(_G["MultiBarRightButton" .. i])
+		hideActionButton(_G["MultiBarLeftButton" .. i])
+		hideActionButton(_G["MultiBar5Button" .. i])
+		hideActionButton(_G["MultiBar6Button" .. i])
+		hideActionButton(_G["MultiBar7Button" .. i])
+	end
 
 	hideActionBarFrame(MicroButtonAndBagsBar, false)
-	--hideActionBarFrame(StanceBar, true)
-	--hideActionBarFrame(PossessActionBar, true)
-	--hideActionBarFrame(MultiCastActionBarFrame, false)
-	--hideActionBarFrame(PetActionBar, true)
+	hideActionBarFrame(StanceBar, true)
+	hideActionBarFrame(PossessActionBar, true)
+	hideActionBarFrame(MultiCastActionBarFrame, false)
+	hideActionBarFrame(PetActionBar, true)
 	hideActionBarFrame(StatusTrackingBarManager, false)
+	--hideActionBarFrame(OverrideActionBar, true)
 
 	-- these events drive visibility, we want the MainMenuBar to remain invisible
 	--MainMenuBar:UnregisterEvent("PLAYER_REGEN_ENABLED")
@@ -106,9 +126,23 @@ Blizzard.HideBlizzard = function(self)
 	--MainMenuBar:UnregisterEvent("ACTIONBAR_SHOWGRID")
 	--MainMenuBar:UnregisterEvent("ACTIONBAR_HIDEGRID")
 
-end
+	ActionBarController:UnregisterAllEvents()
+	ActionBarController:RegisterEvent("SETTINGS_LOADED")
+	ActionBarController:RegisterEvent("UPDATE_EXTRA_ACTIONBAR")
 
-Blizzard.OnInitialize = function(self)
+	if IsAddOnLoaded("Blizzard_NewPlayerExperience") then
+		self:NPE_LoadUI()
+	elseif NPE_LoadUI ~= nil then
+		self:SecureHook("NPE_LoadUI")
+	end
+
+	local HideAlerts = function()
+		if (HelpTip) then
+			HelpTip:HideAllSystem("MicroButtons")
+		end
+	end
+	hooksecurefunc("MainMenuMicroButton_ShowAlert", HideAlerts)
+
 end
 
 Blizzard.OnEnable = function(self)
