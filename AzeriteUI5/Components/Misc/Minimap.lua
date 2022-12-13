1a088a8940507a015d3319629fb0b0a79726ed53
Lars Norberg <github@larsnorbergofficial.com>
2022-12-13 12:19:51 +0100

    first draft of theming

--- AzeriteUI5/Components/Misc/Minimap.lua
+++ AzeriteUI5/Components/Misc/Minimap.lua
@@ -24,14 +24,204 @@
 
 --]]
 local Addon, ns = ...
-local MinimapMod = ns:NewModule("Minimap", "LibMoreEvents-1.0", "AceHook-3.0")
+local MinimapMod = ns:NewModule("Minimap", "LibMoreEvents-1.0", "AceHook-3.0", "AceTimer-3.0", "AceConsole-3.0")
 
-MinimapMod.StyleMap = function(self)
+LoadAddOn("Blizzard_TimeManager")
 
+-- Lua API
+local next = next
+local unpack = unpack
+
+-- Addon API
+local Colors = ns.Colors
+local GetFont = ns.API.GetFont
+local GetMedia = ns.API.GetMedia
+local UIHider = ns.Hider
+
+
+local DEFAULT_THEME = "Blizzard"
+local CURRENT_THEME = DEFAULT_THEME
+
+local Elements = {}
+
+local Objects = {
+	BorderTop = MinimapCluster.BorderTop,
+	Calendar = GameTimeFrame,
+	Clock = TimeManagerClockButton,
+	Compass = MinimapCompassTexture,
+	Difficulty = MinimapCluster.InstanceDifficulty,
+	Expansion = ExpansionLandingPageMinimapButton,
+	Mail = MinimapCluster.MailFrame,
+	Tracking = MinimapCluster.Tracking,
+	Zone = MinimapCluster.ZoneTextButton,
+	ZoomIn = Minimap.ZoomIn,
+	ZoomOut = Minimap.ZoomOut
+}
+
+local ObjectOwners = {
+	BorderTop = MinimapCluster,
+	Calendar = MinimapCluster,
+	Clock = MinimapCluster,
+	Compass = MinimapBackdrop,
+	Difficulty = MinimapCluster,
+	Expansion = MinimapBackdrop,
+	Mail = MinimapCluster,
+	Tracking = MinimapCluster,
+	Zone = MinimapCluster,
+	ZoomIn = Minimap,
+	ZoomOut = Minimap
+}
+
+local ElementTypes = {
+	Backdrop = "Texture",
+	Border = "Texture"
+}
+
+local Shapes = {
+	Round = GetMedia("minimap-mask-opaque"),
+	RoundTransparent = GetMedia("minimap-mask-transparent")
+}
+
+local Skins = {
+	Blizzard = {
+		Version = 1,
+		Shape = "Round"
+	},
+	Azerite = {
+		Version = 1,
+		Shape = "RoundTransparent",
+		HideElements = {
+			BorderTop = true,
+			Calendar = true,
+			Clock = true,
+			Compass = true,
+			Difficulty = true,
+			Expansion = true,
+			Mail = true,
+			Tracking = true,
+			Zone = true,
+			ZoomIn = true,
+			ZoomOut = true
+		},
+		Elements = {
+			Backdrop = {
+				Owner = "Minimap",
+				DrawLayer = "BACKGROUND",
+				DrawLevel = -7,
+				Path = GetMedia("minimap-mask-opaque"),
+				Size = { 198, 198 },
+				Point = { "CENTER" },
+				Color = { 0, 0, 0, .75 },
+			},
+			Border = {
+				Owner = "Backdrop",
+				DrawLayer = "BORDER",
+				DrawLevel = 1,
+				Path = GetMedia("minimap-border"),
+				Size = { 404, 404 },
+				Point = { "CENTER", -1, 0 },
+				Color = { Colors.ui[1], Colors.ui[2], Colors.ui[3] },
+			}
+		}
+	}
+}
+
+local Prototype = {}
+
+Prototype.RegisterTheme = function(self, name, skin)
+	if (Skins[name] or name == DEFAULT_THEME) then return end
+	Skins[name] = skin
+end
+
+Prototype.SetTheme = function(self, name)
+	if (not Skins[name] or name == CURRENT_THEME) then return end
+
+	local current, new = Skins[CURRENT_THEME], Skins[name]
+
+	-- Disable unused elements.
+	if (current.Elements) then
+		for element,data in next,current.Elements do
+			if (not new.Elements or not new.Elements[element]) then
+				Elements[element]:SetParent(UIHider)
+			end
+		end
+	end
+
+	-- Update Blizzard element visibility.
+	for element,object in next,Objects do
+		object:SetParent(new.HideElements and new.HideElements[element] and UIHider or ObjectOwners[element])
+	end
+
+	local mask = new.Shape and Shapes[new.Shape] or Shapes.Round
+	Minimap:SetMaskTexture(mask)
+
+	-- Show new theme's elements.
+	if (new.Elements) then
+		for element,data in next,new.Elements do
+
+			local owner = data.Owner and ObjectOwners[data.Owner] or Minimap
+			local object = Elements[element]
+			if (not object) then
+				if (ElementTypes[element] == "Texture") then
+					object = owner:CreateTexture()
+					Elements[element] = object
+				end
+			end
+
+			-- Silently ignore non-supported objects.
+			if (object) then
+
+				object:SetParent(owner)
+
+				if (data.Size) then
+					object:SetSize(unpack(data.Size))
+				else
+					object:SetSize(Minimap:GetSize())
+				end
+
+				if (data.Point) then
+					object:ClearAllPoints()
+					object:SetPoint(unpack(data.Point))
+				end
+
+				if (ElementTypes[element] == "Texture") then
+					object:SetTexture(data.Path)
+					object:SetDrawLayer(data.DrawLayer or "ARTWORK", data.DrawLevel or 0)
+					if (data.Color) then
+						object:SetVertexColor(unpack(data.Color))
+					else
+						object:SetVertexColor(1, 1, 1, 1)
+					end
+				end
+
+			end
+		end
+	end
+
+	CURRENT_THEME = name
+end
+
+MinimapMod.SetTheme = function(self, theme)
+	local theme = self:GetArgs(string.lower(theme))
+	for name in next,Skins do
+		if (string.lower(name) == theme) then
+			Minimap:SetTheme(name)
+			return
+		end
+	end
+end
+
+MinimapMod.Embed = function(self)
+	for method,func in next,Prototype do
+		_G.Minimap[method] = func
+	end
 end
 
 MinimapMod.OnInitialize = function(self)
+	self:Embed()
+	self:RegisterChatCommand("setminimaptheme", "SetTheme")
 end
 
 MinimapMod.OnEnable = function(self)
+	Minimap:SetTheme("Azerite")
 end
