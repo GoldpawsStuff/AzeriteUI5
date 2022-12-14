be3cc5338f49edce22ec5ec58e912eb2b111a644
Lars Norberg <github@larsnorbergofficial.com>
2022-12-14 13:55:43 +0100

    update settings and profiles

--- AzeriteUI5/Core/Core.lua
+++ AzeriteUI5/Core/Core.lua
@@ -30,14 +30,15 @@ ns.callbacks = LibStub("CallbackHandler-1.0"):New(ns, nil, nil, false)
 ns.Hider = CreateFrame("Frame"); ns.Hider:Hide()
 ns.Noop = function() end
 
+-- Changes will force a full db reset.
+ns.SETTINGS_VERSION = 1
+
+-- Add to global namespace for other addons.
 _G[Addon] = ns
 
--- Version flag to force full setting resets.
-local SETTINGS_VERSION = 1
-
--- Default settings
 local defaults = {
 	profile = {
+		relativeScale = 1
 	}
 }
 
@@ -49,34 +50,12 @@ local next = next
 local string_lower = string.lower
 local tonumber = tonumber
 
--- WoW API
-local InCombatLockdown = InCombatLockdown
-
 -- Addon API
 local SetRelativeScale = ns.API.SetRelativeScale
 local UpdateObjectScales = ns.API.UpdateObjectScales
 local ShowMovableFrameAnchors = ns.Widgets.ShowMovableFrameAnchors
 local HideMovableFrameAnchors = ns.Widgets.HideMovableFrameAnchors
 local ToggleMovableFrameAnchors = ns.Widgets.ToggleMovableFrameAnchors
-
--- Keep custom scales within limits.
-local LimitScale = function(scale)
-	return math_min(1.5, math_max(.5, scale))
-end
-
--- Purge deprecated settings,
--- translate to new where applicable,
--- make sure important ones are within bounds.
-local SanitizeSettings = function(db)
-	if (not db) then
-		return
-	end
-	local scale = db.global.core.relativeScale
-	if (scale) then
-		db.global.core.relativeScale = LimitScale(scale)
-	end
-	return db
-end
 
 -- Proxy method to avoid modules using the callback object directly
 ns.Fire = function(self, name, ...)
@@ -96,38 +75,40 @@ ns.ToggleMovableFrames = function(self)
 end
 
 ns.ResetScale = function(self)
-	if (InCombatLockdown()) then
-		return
-	end
-	local db = self.db
-	local scale = db.global.core.relativeScale
-	local defaultScale = defaults.global.core.relativeScale
-	if (scale and scale ~= defaultScale) then
-		db.global.core.relativeScale = defaultScale -- Store the saved setting
+	if (InCombatLockdown()) then return end
+
+	local scale = self.db.profile.relativeScale
+	local defaultScale = defaults.profile.relativeScale
+
+	if (scale ~= defaultScale) then
+		self.db.profile.relativeScale = defaultScale -- Store the saved setting
+
 		SetRelativeScale(defaultScale) -- Store it in the addon namespace
 		UpdateObjectScales() -- Apply it to existing objects
+
 		-- Fire callbacks to submodules.
-		ns.callbacks:Fire("Relative_Scale_Updated", db.global.core.relativeScale)
+		ns.callbacks:Fire("Relative_Scale_Updated", self.db.profile.relativeScale)
 	end
 end
 
 ns.SetScale = function(self, input)
-	if (InCombatLockdown()) then
-		return
-	end
+	if (InCombatLockdown()) then return end
+
 	local scale = tonumber((self:GetArgs(string_lower(input))))
 	if (scale) then
-		local db = self.db
-		local oldScale = db.global.core.relativeScale
+		local oldScale = self.db.profile.relativeScale
+
 		-- Sanitize it, don't want crazy values
-		scale = LimitScale(scale)
+		scale = math_min(1.5, math_max(.5, scale))
 		if (oldScale ~= scale) then
+
 			-- Store and apply new relative user scale
-			db.global.core.relativeScale = scale -- Store the saved setting
+			self.db.profile.relativeScale = scale -- Store the saved setting
 			SetRelativeScale(scale) -- Store it in the addon namespace
 			UpdateObjectScales() -- Apply it to existing objects
+
 			-- Fire callbacks to submodules.
-			ns.callbacks:Fire("Relative_Scale_Updated", db.global.core.relativeScale)
+			ns.callbacks:Fire("Relative_Scale_Updated", self.db.profile.relativeScale)
 		end
 	end
 end
@@ -139,14 +120,12 @@ end
 
 ns.OnInitialize = function(self)
 
-	self.db = SanitizeSettings(LibStub("AceDB-3.0"):New("AzeriteUI5_DB", defaults, true))
+	self.db = LibStub("AceDB-3.0"):New("AzeriteUI5_DB", defaults, true)
 
-	-- Forcereset settings on version changes.
-	local settings_version = self.db.global.version
-	if (settings_version ~= SETTINGS_VERSION) then
-		self.db:ResetDB()
-
-		self.db.global.version = SETTINGS_VERSION
+	-- Force reset settings on backwards incompatible changes.
+	if (self.db.profile.version ~= ns.SETTINGS_VERSION) then
+		self.db:ResetDB() -- Full db reset of all profiles. Destructive operation.
+		self.db.profile.version = ns.SETTINGS_VERSION -- Store version in default profile.
 	end
 
 	self.db.RegisterCallback(self, "OnProfileChanged", "UpdateSettings")
@@ -154,12 +133,12 @@ ns.OnInitialize = function(self)
 	self.db.RegisterCallback(self, "OnProfileReset", "UpdateSettings")
 
 	-- Apply user scale to all elements
-	--if (self.db.global.core.relativeScale) then
-	--	self.API.SetRelativeScale(self.db.global.core.relativeScale)
-	--end
+	if (self.db.profile.relativeScale) then
+		self.API.SetRelativeScale(self.db.profile.relativeScale)
+	end
 
-	--self:RegisterChatCommand("setscale", "SetScale")
-	--self:RegisterChatCommand("resetscale", "ResetScale")
+	self:RegisterChatCommand("setscale", "SetScale")
+	self:RegisterChatCommand("resetscale", "ResetScale")
 	self:RegisterChatCommand("lock", "LockMovableFrames")
 	self:RegisterChatCommand("unlock", "UnlockMovableFrames")
 	self:RegisterChatCommand("togglelock", "ToggleMovableFrames")
