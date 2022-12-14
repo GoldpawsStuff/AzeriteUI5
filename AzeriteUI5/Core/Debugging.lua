83c39157a5ce662074bfd6176e2897da042431c4
Lars Norberg <github@larsnorbergofficial.com>
2022-12-14 13:27:58 +0100

    only retail here

--- AzeriteUI5/Core/Debugging.lua
+++ AzeriteUI5/Core/Debugging.lua
@@ -36,7 +36,7 @@ local string_format = string.format
 local EnableAddOn = EnableAddOn
 local GetAddOnInfo = GetAddOnInfo
 
-local ADDONS = (ns.IsRetail) and {
+local ADDONS = {
 
 	"Blizzard_AchievementUI",
 	"Blizzard_AdventureMap",
@@ -157,65 +157,9 @@ local ADDONS = (ns.IsRetail) and {
 	"Blizzard_WorldMap",
 	"Blizzard_WowTokenUI"
 
-} or (ns.IsWrath) and {
-
-	"Blizzard_AchievementUI",
-	"Blizzard_APIDocumentation",
-	"Blizzard_APIDocumentationGenerated",
-	"Blizzard_ArenaUI",
-	"Blizzard_AuctionUI",
-	"Blizzard_AuthChallengeUI",
-	"Blizzard_BarbershopUI",
-	"Blizzard_BattlefieldMap",
-	"Blizzard_BehavioralMessaging",
-	"Blizzard_BindingUI",
-	"Blizzard_Calendar",
-	"Blizzard_Channels",
-	"Blizzard_ClientSavedVariables",
-	"Blizzard_CombatLog",
-	"Blizzard_CombatText",
-	"Blizzard_Commentator",
-	"Blizzard_Communities",
-	"Blizzard_CompactRaidFrames",
-	"Blizzard_Console",
-	"Blizzard_CraftUI",
-	"Blizzard_CUFProfiles",
-	"Blizzard_DebugTools",
-	"Blizzard_Deprecated",
-	"Blizzard_EventTrace",
-	"Blizzard_FrameEffects",
-	"Blizzard_GlyphUI",
-	"Blizzard_GMChatUI",
-	"Blizzard_GuildBankUI",
-	"Blizzard_InspectUI",
-	"Blizzard_ItemSocketingUI",
-	"Blizzard_Kiosk",
-	"Blizzard_LookingForGroupUI",
-	"Blizzard_MacroUI",
-	"Blizzard_MapCanvas",
-	"Blizzard_MovePad",
-	"Blizzard_NamePlates",
-	"Blizzard_PTRFeedback",
-	"Blizzard_RaidUI",
-	"Blizzard_SecureTransferUI",
-	"Blizzard_SharedMapDataProviders",
-	"Blizzard_SocialUI",
-	"Blizzard_StoreUI",
-	"Blizzard_TalentUI",
-	"Blizzard_TimeManager",
-	"Blizzard_TokenUI",
-	"Blizzard_TradeSkillUI",
-	"Blizzard_TrainerUI",
-	"Blizzard_UIWidgets",
-	"Blizzard_WorldMap",
-	"Blizzard_WowTokenUI"
-
 }
 
 Debugging.EnableBlizzardAddOns = function(self)
-	if (not ADDONS) then
-		return
-	end
 	local disabled = {}
 	for _,addon in next,ADDONS do
 		local reason = select(5, GetAddOnInfo(addon))
@@ -236,11 +180,10 @@ Debugging.EnableBlizzardAddOns = function(self)
 		for _,addon in next,ADDONS do
 			print(string_format("|cfff0f0f0%s|r", addon))
 		end
+		print("|cfff00a0aA /reload is required to apply changes!|r")
 	end
 end
 
 Debugging.OnInitialize = function(self)
-	for _,cmd in next, { "enableblizz", "enableblizzard", "fixblizz", "fixblizzard" } do
-		self:RegisterChatCommand(cmd, "EnableBlizzardAddOns")
-	end
+	self:RegisterChatCommand("enableblizzard", "EnableBlizzardAddOns")
 end
