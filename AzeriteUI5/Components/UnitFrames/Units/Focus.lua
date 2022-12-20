70a82ae500a2334431a725253532815029359b60
Lars Norberg <github@larsnorbergofficial.com>
2022-12-20 13:46:05 +0100

    add focus frame

--- AzeriteUI5/Components/UnitFrames/Units/Focus.lua
+++ AzeriteUI5/Components/UnitFrames/Units/Focus.lua
@@ -24,3 +24,466 @@
 
 --]]
 local Addon, ns = ...
+local oUF = ns.oUF
+
+local FocusMod = ns:Merge(ns:NewModule("FocusFrame", "LibMoreEvents-1.0"), ns.UnitFrame.modulePrototype)
+
+-- Lua API
+local unpack = unpack
+local string_gsub = string.gsub
+
+-- Addon API
+local Colors = ns.Colors
+local GetFont = ns.API.GetFont
+local GetMedia = ns.API.GetMedia
+
+-- Constants
+local playerClass = ns.PlayerClass
+
+local defaults = { profile = ns:Merge({
+	enabled = true,
+	savedPosition = {
+		Azerite = {
+			scale = 1,
+			[1] = "BOTTOMLEFT",
+			[2] = 332-136,
+			[3] = 270-23
+		}
+	}
+}, ns.UnitFrame.defaults) }
+
+local barSparkMap = {
+	top = {
+		{ keyPercent =   0/128, offset = -16/32 },
+		{ keyPercent =   4/128, offset = -16/32 },
+		{ keyPercent =  10/128, offset =   0/32 },
+		{ keyPercent = 119/128, offset =   0/32 },
+		{ keyPercent = 126/128, offset = -16/32 },
+		{ keyPercent = 128/128, offset = -16/32 }
+	},
+	bottom = {
+		{ keyPercent =   0/128, offset = -16/32 },
+		{ keyPercent =   4/128, offset = -16/32 },
+		{ keyPercent =  10/128, offset =   0/32 },
+		{ keyPercent = 119/128, offset =   0/32 },
+		{ keyPercent = 126/128, offset = -16/32 },
+		{ keyPercent = 128/128, offset = -16/32 }
+	}
+}
+
+local config = {
+
+	Size = { 136, 47 },
+	Position = { "RIGHT", UIParent, "BOTTOMLEFT", 332-136, 270-23 },
+	HitRectInsets = { 0, 0, 0, 0 },
+
+	HealthBarPosition = { "CENTER", 0, 0 },
+	HealthBarSize = { 112, 11 },
+	HealthBarTexture = GetMedia("cast_bar"),
+	HealthBarOrientation = "RIGHT",
+	HealthBarSparkMap = barSparkMap,
+	HealthAbsorbColor = { 1, 1, 1, .5 },
+	HealthCastOverlayColor = { 1, 1, 1, .5 },
+
+	HealthBackdropPosition = { "CENTER", 1, -2 },
+	HealthBackdropSize = { 193,93 },
+	HealthBackdropTexture = GetMedia("cast_back"),
+	HealthBackdropColor = { Colors.ui[1], Colors.ui[2], Colors.ui[3] },
+
+	HealthValuePosition = { "CENTER", 0, 0 },
+	HealthValueJustifyH = "CENTER",
+	HealthValueJustifyV = "MIDDLE",
+	HealthValueFont = GetFont(14, true),
+	HealthValueColor = { Colors.offwhite[1], Colors.offwhite[2], Colors.offwhite[3], .75 },
+
+	NamePosition = { "BOTTOMLEFT", 12, 46 },
+	NameJustifyH = "LEFT",
+	NameJustifyV = "TOP",
+	NameFont = GetFont(14, true),
+	NameColor = { Colors.highlight[1], Colors.highlight[2], Colors.highlight[3], .75 },
+
+	TargetHighlightPosition = { "CENTER", 1, -2 },
+	TargetHighlightSize = { 193,93 },
+	TargetHighlightTexture = GetMedia("cast_back_outline"),
+	TargetHighlightTargetColor = { 255/255, 239/255, 169/255, 1 },
+	TargetHighlightFocusColor = { 144/255, 195/255, 255/255, 1 },
+
+}
+
+-- Utility Functions
+--------------------------------------------
+-- Simplify the tagging process a little.
+local prefix = function(msg)
+	return string_gsub(msg, "*", ns.Prefix)
+end
+
+-- Element Callbacks
+--------------------------------------------
+-- Forceupdate health prediction on health updates,
+-- to assure our smoothed elements are properly aligned.
+local Health_PostUpdate = function(element, unit, cur, max)
+	local predict = element.__owner.HealthPrediction
+	if (predict) then
+		predict:ForceUpdate()
+	end
+end
+
+-- Update the health preview color on health color updates.
+local Health_PostUpdateColor = function(element, unit, r, g, b)
+	local preview = element.Preview
+	if (preview) then
+		preview:SetStatusBarColor(r * .7, g * .7, b * .7)
+	end
+end
+
+-- Align our custom health prediction texture
+-- based on the plugin's provided values.
+local HealPredict_PostUpdate = function(element, unit, myIncomingHeal, otherIncomingHeal, absorb, healAbsorb, hasOverAbsorb, hasOverHealAbsorb, curHealth, maxHealth)
+
+	local allIncomingHeal = myIncomingHeal + otherIncomingHeal
+	local allNegativeHeals = healAbsorb
+	local showPrediction, change
+
+	if ((allIncomingHeal > 0) or (allNegativeHeals > 0)) and (maxHealth > 0) then
+		local startPoint = curHealth/maxHealth
+
+		-- Dev switch to test absorbs with normal healing
+		--allIncomingHeal, allNegativeHeals = allNegativeHeals, allIncomingHeal
+
+		-- Hide predictions if the change is very small, or if the unit is at max health.
+		change = (allIncomingHeal - allNegativeHeals)/maxHealth
+		if ((curHealth < maxHealth) and (change > (element.health.predictThreshold or .05))) then
+			local endPoint = startPoint + change
+
+			-- Crop heal prediction overflows
+			if (endPoint > 1) then
+				endPoint = 1
+				change = endPoint - startPoint
+			end
+
+			-- Crop heal absorb overflows
+			if (endPoint < 0) then
+				endPoint = 0
+				change = -startPoint
+			end
+
+			-- This shouldn't happen, but let's do it anyway.
+			if (startPoint ~= endPoint) then
+				showPrediction = true
+			end
+		end
+	end
+
+	if (showPrediction) then
+
+		local preview = element.preview
+		local growth = preview:GetGrowth()
+		local min,max = preview:GetMinMaxValues()
+		local value = preview:GetValue() / max
+		local previewTexture = preview:GetStatusBarTexture()
+		local previewWidth, previewHeight = preview:GetSize()
+		local left, right, top, bottom = preview:GetTexCoord()
+		local isFlipped = preview:IsFlippedHorizontally()
+
+		if (growth == "RIGHT") then
+
+			local texValue, texChange = value, change
+			local rangeH, rangeV
+
+			rangeH = right - left
+			rangeV = bottom - top
+			texChange = change*value
+			texValue = left + value*rangeH
+
+			if (change > 0) then
+				element:ClearAllPoints()
+				element:SetPoint("BOTTOMLEFT", previewTexture, "BOTTOMRIGHT", 0, 0)
+				element:SetSize(change*previewWidth, previewHeight)
+				if (isFlipped) then
+					element:SetTexCoord(texValue + texChange, texValue, top, bottom)
+				else
+					element:SetTexCoord(texValue, texValue + texChange, top, bottom)
+				end
+				element:SetVertexColor(0, .7, 0, .25)
+				element:Show()
+
+			elseif (change < 0) then
+				element:ClearAllPoints()
+				element:SetPoint("BOTTOMRIGHT", previewTexture, "BOTTOMRIGHT", 0, 0)
+				element:SetSize((-change)*previewWidth, previewHeight)
+				if (isFlipped) then
+					element:SetTexCoord(texValue, texValue + texChange, top, bottom)
+				else
+					element:SetTexCoord(texValue + texChange, texValue, top, bottom)
+				end
+				element:SetVertexColor(.5, 0, 0, .75)
+				element:Show()
+
+			else
+				element:Hide()
+			end
+
+		elseif (growth == "LEFT") then
+			local texValue, texChange = value, change
+			local rangeH, rangeV
+			rangeH = right - left
+			rangeV = bottom - top
+			texChange = change*value
+			texValue = left + value*rangeH
+
+			if (change > 0) then
+				element:ClearAllPoints()
+				element:SetPoint("BOTTOMRIGHT", previewTexture, "BOTTOMLEFT", 0, 0)
+				element:SetSize(change*previewWidth, previewHeight)
+				if (isFlipped) then
+					element:SetTexCoord(texValue, texValue + texChange, top, bottom)
+				else
+					element:SetTexCoord(texValue + texChange, texValue, top, bottom)
+				end
+				element:SetVertexColor(0, .7, 0, .25)
+				element:Show()
+
+			elseif (change < 0) then
+				element:ClearAllPoints()
+				element:SetPoint("BOTTOMLEFT", previewTexture, "BOTTOMLEFT", 0, 0)
+				element:SetSize((-change)*previewWidth, previewHeight)
+				if (isFlipped) then
+					element:SetTexCoord(texValue + texChange, texValue, top, bottom)
+				else
+					element:SetTexCoord(texValue, texValue + texChange, top, bottom)
+				end
+				element:SetVertexColor(.5, 0, 0, .75)
+				element:Show()
+
+			else
+				element:Hide()
+			end
+		end
+	else
+		element:Hide()
+	end
+
+	local absorb = element.Absorb
+	if (absorb) then
+		local fraction = absorb/maxHealth
+		if (fraction > .6) then
+			absorb = maxHealth * .6
+		end
+		absorb:SetMinMaxValues(0, maxHealth)
+		absorb:SetValue(absorb)
+	end
+
+end
+
+-- Update targeting highlight outline
+local TargetHighlight_Update = function(self, event, unit, ...)
+	if (unit and unit ~= self.unit) then return end
+
+	local element = self.TargetHighlight
+	unit = unit or self.unit
+
+	if (UnitIsUnit(unit, "target")) then
+		element:SetVertexColor(unpack(element.colorTarget))
+		element:Show()
+	else
+		element:Hide()
+	end
+
+end
+
+local UnitFrame_PostUpdate = function(self)
+	TargetHighlight_Update(self)
+end
+
+local UnitFrame_OnEvent = function(self, event, unit, ...)
+	UnitFrame_PostUpdate(self)
+end
+
+local style = function(self, unit)
+
+	local db = config
+
+	self:SetSize(unpack(db.Size))
+	self:SetHitRectInsets(unpack(db.HitRectInsets))
+	self:SetFrameLevel(self:GetFrameLevel() + 10)
+
+	-- Overlay for icons and text
+	--------------------------------------------
+	local overlay = CreateFrame("Frame", nil, self)
+	overlay:SetFrameLevel(self:GetFrameLevel() + 7)
+	overlay:SetAllPoints()
+
+	self.Overlay = overlay
+
+	-- Health
+	--------------------------------------------
+	local health = self:CreateBar()
+	health:SetFrameLevel(health:GetFrameLevel() + 2)
+	health:SetPoint(unpack(db.HealthBarPosition))
+	health:SetSize(unpack(db.HealthBarSize))
+	health:SetStatusBarTexture(db.HealthBarTexture)
+	health:SetOrientation(db.HealthBarOrientation)
+	health:SetSparkMap(db.HealthBarSparkMap)
+	health.predictThreshold = .01
+	health.colorDisconnected = true
+	health.colorTapping = true
+	health.colorThreat = true
+	health.colorClass = true
+	health.colorClassPet = true
+	health.colorHappiness = true
+	health.colorReaction = true
+
+	self.Health = health
+	self.Health.Override = ns.API.UpdateHealth
+	self.Health.PostUpdate = Health_PostUpdate
+	self.Health.PostUpdateColor = Health_PostUpdateColor
+
+	local healthBackdrop = health:CreateTexture(nil, "BACKGROUND", nil, -1)
+	healthBackdrop:SetPoint(unpack(db.HealthBackdropPosition))
+	healthBackdrop:SetSize(unpack(db.HealthBackdropSize))
+	healthBackdrop:SetTexture(db.HealthBackdropTexture)
+	healthBackdrop:SetVertexColor(unpack(db.HealthBackdropColor))
+
+	self.Health.Backdrop = healthBackdrop
+
+	local healthPreview = self:CreateBar(nil, health)
+	healthPreview:SetAllPoints(health)
+	healthPreview:SetFrameLevel(health:GetFrameLevel() - 1)
+	healthPreview:SetStatusBarTexture(db.HealthBarTexture)
+	healthPreview:SetOrientation(db.HealthBarOrientation)
+	healthPreview:SetSparkTexture("")
+	healthPreview:SetAlpha(.5)
+	healthPreview:DisableSmoothing(true)
+
+	self.Health.Preview = healthPreview
+
+	-- Health Prediction
+	--------------------------------------------
+	local healPredictFrame = CreateFrame("Frame", nil, health)
+	healPredictFrame:SetFrameLevel(health:GetFrameLevel() + 2)
+
+	local healPredict = healPredictFrame:CreateTexture(nil, "OVERLAY", nil, 1)
+	healPredict:SetTexture(db.HealthBarTexture)
+	healPredict.health = health
+	healPredict.preview = healthPreview
+	healPredict.maxOverflow = 1
+
+	self.HealthPrediction = healPredict
+	self.HealthPrediction.PostUpdate = HealPredict_PostUpdate
+
+	-- Cast Overlay
+	--------------------------------------------
+	local castbar = self:CreateBar()
+	castbar:SetAllPoints(health)
+	castbar:SetFrameLevel(self:GetFrameLevel() + 5)
+	castbar:SetSparkMap(db.HealthBarSparkMap)
+	castbar:SetStatusBarTexture(db.HealthBarTexture)
+	castbar:SetStatusBarColor(unpack(db.HealthCastOverlayColor))
+	castbar:DisableSmoothing(true)
+
+	self.Castbar = castbar
+
+	-- Health Value
+	--------------------------------------------
+	local healthValue = health:CreateFontString(nil, "OVERLAY", nil, 1)
+	healthValue:SetPoint(unpack(db.HealthValuePosition))
+	healthValue:SetFontObject(db.HealthValueFont)
+	healthValue:SetTextColor(unpack(db.HealthValueColor))
+	healthValue:SetJustifyH(db.HealthValueJustifyH)
+	healthValue:SetJustifyV(db.HealthValueJustifyV)
+	self:Tag(healthValue, prefix("[*:Health(true)]"))
+
+	self.Health.Value = healthValue
+
+	-- Unit Name
+	--------------------------------------------
+	local name = self:CreateFontString(nil, "OVERLAY", nil, 1)
+	name:SetPoint(unpack(db.NamePosition))
+	name:SetFontObject(db.NameFont)
+	name:SetTextColor(unpack(db.NameColor))
+	name:SetJustifyH(db.NameJustifyH)
+	name:SetJustifyV(db.NameJustifyV)
+	self:Tag(name, prefix("[*:Name(16,nil,nil,true)]"))
+
+	self.Name = name
+
+	-- Absorb Bar (Retail)
+	--------------------------------------------
+	if (ns.IsRetail) then
+		local absorb = self:CreateBar()
+		absorb:SetAllPoints(health)
+		absorb:SetFrameLevel(health:GetFrameLevel() + 3)
+		absorb:SetStatusBarTexture(db.HealthBarTexture)
+		absorb:SetStatusBarColor(unpack(db.HealthAbsorbColor))
+		absorb:SetSparkMap(db.HealthBarSparkMap)
+
+		local orientation
+		if (db.HealthBarOrientation == "UP") then
+			orientation = "DOWN"
+		elseif (db.HealthBarOrientation == "DOWN") then
+			orientation = "UP"
+		elseif (db.HealthBarOrientation == "LEFT") then
+			orientation = "RIGHT"
+		else
+			orientation = "LEFT"
+		end
+		absorb:SetOrientation(orientation)
+
+		self.Health.Absorb = absorb
+	end
+
+	-- Target Highlight
+	--------------------------------------------
+	local targetHighlight = overlay:CreateTexture(nil, "BACKGROUND", nil, -2)
+	targetHighlight:SetPoint(unpack(db.TargetHighlightPosition))
+	targetHighlight:SetSize(unpack(db.TargetHighlightSize))
+	targetHighlight:SetTexture(db.TargetHighlightTexture)
+	targetHighlight.colorTarget = db.TargetHighlightTargetColor
+	targetHighlight.colorFocus = db.TargetHighlightFocusColor
+
+	self.TargetHighlight = targetHighlight
+
+	-- Textures need an update when frame is displayed.
+	self.PostUpdate = UnitFrame_PostUpdate
+
+	-- Register events to handle additional texture updates.
+	self:RegisterEvent("PLAYER_ENTERING_WORLD", UnitFrame_OnEvent, true)
+	self:RegisterEvent("PLAYER_TARGET_CHANGED", UnitFrame_OnEvent, true)
+
+end
+
+FocusMod.Spawn = function(self)
+
+	-- UnitFrame
+	---------------------------------------------------
+	local unit, name = "focus", "Focus"
+
+	oUF:RegisterStyle(ns.Prefix..name, style)
+	oUF:SetActiveStyle(ns.Prefix..name)
+
+	self.frame = ns.UnitFrame.Spawn(unit, ns.Prefix.."UnitFrame"..name)
+
+	-- Movable Frame Anchor
+	---------------------------------------------------
+	local anchor = ns.Widgets.RequestMovableFrameAnchor()
+	anchor:SetTitle(FOCUS) -- crazy long
+	anchor:SetScalable(true)
+	anchor:SetMinMaxScale(.75, 1.25, .05)
+	anchor:SetSize(136, 47)
+	anchor:SetPoint(unpack(self.defaults.profile.savedPosition.Azerite))
+	anchor:SetScale(self.defaults.profile.savedPosition.Azerite.scale)
+	anchor.frameOffsetX = 0
+	anchor.frameOffsetY = 0
+	anchor.framePoint = "BOTTOMLEFT"
+	anchor.Callback = function(anchor, ...) self:OnAnchorUpdate(...) end
+
+	self.anchor = anchor
+end
+
+FocusMod.OnInitialize = function(self)
+	self.db = ns.db:RegisterNamespace("FocusFrame", defaults)
+	self.defaults = defaults
+	self:SetEnabledState(self.db.profile.enabled)
+
+	oUF:DisableBlizzard("targettarget")
+end
