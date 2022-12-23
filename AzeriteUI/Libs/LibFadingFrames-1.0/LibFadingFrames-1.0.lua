9754d6713820f0e32d9ae6ad944ebea551bd5328
Lars Norberg <github@larsnorbergofficial.com>
2022-12-23 12:18:45 +0100

    version 1

--- AzeriteUI/Libs/LibFadingFrames-1.0/LibFadingFrames-1.0.lua
+++ AzeriteUI/Libs/LibFadingFrames-1.0/LibFadingFrames-1.0.lua
@@ -24,7 +24,7 @@
 
 --]]
 local MAJOR_VERSION = "LibFadingFrames-1.0"
-local MINOR_VERSION = -1
+local MINOR_VERSION = 1
 
 assert(LibStub, MAJOR_VERSION .. " requires LibStub.")
 
@@ -34,6 +34,9 @@ assert(MAJOR_VERSION .. " requires LibMoreEvents-1.0.")
 local AceTimer = LibStub:GetLibrary("AceTimer-3.0", true)
 assert(MAJOR_VERSION .. " requires AceTimer-3.0.")
 
+local AceHook = LibStub:GetLibrary("AceHook-3.0", true)
+assert(MAJOR_VERSION .. " requires AceHook-3.0.")
+
 local lib, oldversion = LibStub:NewLibrary(MAJOR_VERSION, MINOR_VERSION)
 if (not lib) then
 	return
@@ -41,9 +44,11 @@ end
 
 LibMoreEvents:Embed(lib)
 AceTimer:Embed(lib)
+AceHook:Embed(lib)
 
 lib.fadeFrames = lib.fadeFrames or {}
 lib.fadeFrameType = lib.fadeFrameType or {}
+lib.fadeFrameHitRects = lib.fadeFrameHitRects or {}
 lib.hoverFrames = lib.hoverFrames or {}
 lib.hoverCount = lib.hoverCount or { default = 0 }
 lib.gridCounter = lib.gridCounter or 0
@@ -56,21 +61,29 @@ local next = next
 -- Frame Metamethods
 local setAlpha = getmetatable(CreateFrame("Frame")).__index.SetAlpha
 
-lib.UpdateFadeFrames = function(self)
-	if (not self.inWorld) then return end
+lib.UpdateFadeFrame = function(self, frame)
 
-	-- Something is forcing the frames to be shown,
-	-- like an item currently on the cursor.
-	if (self.gridCounter >= 1) then
-		for frame in next,self.fadeFrames do
+	local isActionButton = self.fadeFrameType[frame] == "actionbutton"
+	if (isActionButton and self.inCombat or self.gridCounter >= 1) then
+		setAlpha(frame, 1)
+		return
+	end
+
+	if (not self.enableFading) then
+		if (isActionButton) then
+			-- The frame has an action or grid set to visible.
+			if (frame:HasAction()) or (frame.config and frame.config.showGrid) or (frame.parent and frame.parent.config.showGrid) then
+				setAlpha(frame, 1)
+			else
+				setAlpha(frame, 0)
+			end
+		else
 			setAlpha(frame, 1)
 		end
-
-	-- Bar fading is disabled, just copying the default settings here.
-	elseif (not self.enableFading) then
-		for frame in next,self.fadeFrames do
-
-			if (self.fadeFrameType[frame] == "actionbutton") then
+	else
+		-- The group is visible.
+		if (self.hoverCount[self.fadeFrames[frame]] > 0) then
+			if (isActionButton) then
 				-- The frame has an action or grid set to visible.
 				if (frame:HasAction()) or (frame.config and frame.config.showGrid) or (frame.parent and frame.parent.config.showGrid) then
 					setAlpha(frame, 1)
@@ -80,30 +93,17 @@ lib.UpdateFadeFrames = function(self)
 			else
 				setAlpha(frame, 1)
 			end
+		else
+			-- Group is hidden, hide this.
+			setAlpha(frame, 0)
 		end
-	else
-		-- Bar fading is enabled.
-		for frame,fadeGroups in next,self.fadeFrames do
+	end
+end
 
-			-- The group is visible.
-			if (self.hoverCount[fadeGroups] > 0) then
-
-				if (self.fadeFrameType[frame] == "actionbutton") then
-					-- The frame has an action or grid set to visible.
-					if (frame:HasAction()) or (frame.config and frame.config.showGrid) or (frame.parent and frame.parent.config.showGrid) then
-						setAlpha(frame, 1)
-					else
-						setAlpha(frame, 0)
-					end
-				else
-					setAlpha(frame, 1)
-				end
-
-			else
-				-- Group is hidden, hide this.
-				setAlpha(frame, 0)
-			end
-		end
+lib.UpdateFadeFrames = function(self)
+	if (not self.inWorld) then return end
+	for frame in next,self.fadeFrames do
+		self:UpdateFadeFrame(frame)
 	end
 end
 
@@ -120,66 +120,80 @@ lib.OnFadeFrameLeave = function(self, frame, fadeGroup)
 end
 
 lib.CheckFadeFrames = function(self)
+	local needupdate
 	for frame,fadeGroup in next,self.hoverFrames do
-		if (not frame:IsMouseOver()) then
+		-- Frame could've been unregistered while hovered.
+		if (not self.fadeFrames[frame] or not frame:IsMouseOver(unpack(lib.fadeFrameHitRects[frame]))) then
 			self:OnFadeFrameLeave(frame, fadeGroup)
+			needupdate = true
 		end
 	end
 	for frame,fadeGroup in next,self.fadeFrames do
-		if (not self.hoverFrames[frame] and frame:IsMouseOver()) then
+		if (not self.hoverFrames[frame] and frame:IsMouseOver(unpack(lib.fadeFrameHitRects[frame]))) then
 			self:OnFadeFrameEnter(frame, fadeGroup)
+			needupdate = true
 		end
+	end
+	if (needupdate) then
+		self:UpdateFadeFrames()
 	end
 end
 
-lib.RegisterFrameForFading = function(self, frame, fadeGroup)
-	if (self.fadeFrames[frame]) then
+lib.RegisterFrameForFading = function(self, frame, fadeGroup, ...)
+	if (lib.fadeFrames[frame]) then
 		return
 	end
 
-	local shouldInit = not not next(self.fadeFrames)
+	local shouldInit = not next(lib.fadeFrames)
 
 	fadeGroup = fadeGroup or "default"
 
 	-- Not the best check ever, but it'll have to do for now.
 	if (frame:GetObjectType() == "CheckButton") and (frame.HasAction) then
-		self.fadeFrameType[frame] = "actionbutton"
+		lib.fadeFrameType[frame] = "actionbutton"
 	end
 
 	-- Might be spammy, but I prefer not to replace frame methods.
-	self:SecureHook(frame, "SetAlpha", "UpdateFadeFrames")
+	lib:SecureHook(frame, "SetAlpha", "UpdateFadeFrame")
 
-	if (not self.hoverCount[fadeGroup]) then
-		self.hoverCount[fadeGroup] = 0
+	if (not lib.hoverCount[fadeGroup]) then
+		lib.hoverCount[fadeGroup] = 0
 	end
 
-	self.fadeFrames[frame] = fadeGroup
+	lib.fadeFrames[frame] = fadeGroup
+
+	-- Convert frame hit rects to mouseover values.
+	if (...) then
+		local left, right, top, bottom = ...
+		lib.fadeFrameHitRects[frame] = { -top, bottom, left, -right }
+	end
 
 	if (shouldInit) then
-		self:Enable()
+		lib:Enable()
 	end
 
-	self:UpdateFadeFrames()
+	lib:UpdateFadeFrames()
 end
 
 lib.UnregisterFrameForFading = function(self, frame)
-	if (not self.fadeFrames[frame]) then
+	if (not lib.fadeFrames[frame]) then
 		return
 	end
 
-	self:Unhook(frame, "SetAlpha", "UpdateFadeFrames")
+	lib:Unhook(frame, "SetAlpha", "UpdateFadeFrame")
 
-	self.fadeFrames[frame] = nil
-	self.fadeFrameType[frame] = nil
+	lib.fadeFrames[frame] = nil
+	lib.fadeFrameType[frame] = nil
+	lib.fadeFrameHitRects[frame] = nil
 
-	if (not next(self.fadeFrames)) then
-		self:Disable()
+	if (not next(lib.fadeFrames)) then
+		lib:Disable()
 	end
 
-	self:UpdateFadeFrames()
+	lib:UpdateFadeFrames()
 end
 
-lib.EnableFading = function(self)
+lib.Enable = function(self)
 	if (self.enableFading) then return end
 
 	self.enableFading = true
@@ -202,7 +216,7 @@ lib.EnableFading = function(self)
 	self:UpdateFadeFrames()
 end
 
-lib.DisableFading = function(self)
+lib.Disable = function(self)
 	if (not self.enableFading) then return end
 
 	self.enableFading = false
@@ -214,7 +228,7 @@ lib.DisableFading = function(self)
 	self:UnregisterEvent("ACTIONBAR_HIDEGRID", "OnEvent")
 
 	if (self.checkTimer) then
-		self.checkTimer:CancelTimer()
+		self:CancelTimer(self.checkTimer)
 	end
 
 	self:UpdateFadeFrames()
