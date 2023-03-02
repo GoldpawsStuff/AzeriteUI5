822eaf9752e3bebebd6b11c79df084930eac0da7
Lars Norberg <github@larsnorbergofficial.com>
2023-03-02 01:16:35 +0100

    rename tracker files for consistency

---
+++ AzeriteUI/Components/Misc/TrackerVanilla.lua
@@ -0,0 +1,413 @@
+--[[
+
+	The MIT License (MIT)
+
+	Copyright (c) 2023 Lars Norberg
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+
+--]]
+local Addon, ns = ...
+if (not ns.IsClassic) then return end
+
+local Tracker = ns:NewModule("Tracker", "LibMoreEvents-1.0", "AceHook-3.0")
+local MFM = ns:GetModule("MovableFramesManager", true)
+
+-- Lua
+local math_min = math.min
+local unpack = unpack
+
+-- Addon API
+local Colors = ns.Colors
+local GetFont = ns.API.GetFont
+local GetMedia = ns.API.GetMedia
+local IsAddOnEnabled = ns.API.IsAddOnEnabled
+
+local defaults = { profile = ns:Merge({
+	enabled = true,
+	savedPosition = {
+		Azerite = {
+			scale = 1,
+			[1] = "BOTTOMRIGHT",
+			[2] = -60,
+			[3] = 380
+		}
+	}
+}, ns.moduleDefaults) }
+
+local config = {
+	-- Size of the holder. Set to same width as our Minimap.
+	-- *Classic tracker is 280, our size should be 255
+	Size = { 306, 22 },
+	BottomOffset = 380,
+	TopOffset = 260,
+	TrackerWidth = 255,
+	TrackerHeight = 1080 - 380 - 260,
+	FontObject = GetFont(13, true),
+	FontObjectTitle = GetFont(15, true)
+}
+
+Tracker.InitializeTracker = function(self)
+
+	 local frame = CreateFrame("Frame", nil, UIParent)
+	 self.frame = frame
+
+	-- Re-position after UIParent messes with it.
+	hooksecurefunc(QuestWatchFrame, "SetPoint", function(_,_, anchor)
+		if (anchor ~= frame) then
+			self:UpdateTrackerPosition()
+		end
+	end)
+
+	-- Just in case some random addon messes with it.
+	hooksecurefunc(QuestWatchFrame, "SetAllPoints", function()
+		self:UpdateTrackerPosition()
+	end)
+
+	local dummyLine = tracker:CreateFontString()
+	dummyLine:SetFontObject(config.FontObject)
+	dummyLine:SetWidth(config.TrackerWidth)
+	dummyLine:SetJustifyH("RIGHT")
+	dummyLine:SetJustifyV("BOTTOM")
+	dummyLine:SetIndentedWordWrap(false)
+	dummyLine:SetWordWrap(true)
+	dummyLine:SetNonSpaceWrap(false)
+	dummyLine:SetSpacing(0)
+
+	local title = QuestWatchQuestName
+	title:ClearAllPoints()
+	title:SetPoint("TOPRIGHT", tracker, "TOPRIGHT", 0, 0)
+
+	-- Hook line styling
+	hooksecurefunc("QuestWatch_Update", function()
+		local questIndex
+		local numObjectives
+		local watchText
+		local watchTextIndex = 1
+		local objectivesCompleted
+		local text, type, finished
+
+		for i = 1, GetNumQuestWatches() do
+			questIndex = GetQuestIndexForWatch(i)
+			if (questIndex) then
+				numObjectives = GetNumQuestLeaderBoards(questIndex)
+				if (numObjectives > 0) then
+					-- Set quest title
+					watchText = _G["QuestWatchLine"..watchTextIndex]
+					watchText.isTitle = true
+
+					-- Kill trailing nonsense
+					text = watchText:GetText() or ""
+					text = string_gsub(text, "%.$", "")
+					text = string_gsub(text, "%?$", "")
+					text = string_gsub(text, "%!$", "")
+					watchText:SetText(text)
+
+					-- Align the quest title better
+					if (watchTextIndex == 1) then
+						watchText:ClearAllPoints()
+						watchText:SetPoint("TOPRIGHT", title, "TOPRIGHT", 0, -4)
+					else
+						watchText:ClearAllPoints()
+						watchText:SetPoint("TOPRIGHT", _G["QuestWatchLine"..(watchTextIndex - 1)], "BOTTOMRIGHT", 0, -10)
+					end
+					watchTextIndex = watchTextIndex + 1
+
+					-- Style the objectives
+					objectivesCompleted = 0
+					for j = 1, numObjectives do
+
+						-- Set Objective text
+						text, type, finished = GetQuestLogLeaderBoard(j, questIndex)
+						watchText = _G["QuestWatchLine"..watchTextIndex]
+						watchText.isTitle = nil
+
+						-- Kill trailing nonsense
+						text = string_gsub(text, "%.$", "")
+						text = string_gsub(text, "%?$", "")
+						text = string_gsub(text, "%!$", "")
+
+						local objectiveText, minCount, maxCount = string_match(text, "(.+): (%d+)/(%d+)")
+						if (objectiveText and minCount and maxCount) then
+							minCount = tonumber(minCount)
+							maxCount = tonumber(maxCount)
+							if (minCount and maxCount) then
+								if (minCount == maxCount) then
+									text = Colors.quest.green.colorCode .. minCount .. "/" .. maxCount .. "|r " .. objectiveText
+								elseif (maxCount > 0) and (minCount/maxCount >= 2/3 ) then
+									text = Colors.quest.yellow.colorCode .. minCount .. "/" .. maxCount .. "|r " .. objectiveText
+								elseif (maxCount > 0) and (minCount/maxCount >= 1/3 ) then
+									text = Colors.quest.orange.colorCode .. minCount .. "/" .. maxCount .. "|r " .. objectiveText
+								else
+									text = Colors.quest.red.colorCode .. minCount .. "/" .. maxCount .. "|r " .. objectiveText
+								end
+							end
+						end
+						watchText:SetText(text)
+
+						-- Color the objectives
+						if (finished) then
+							watchText:SetTextColor(Colors.highlight[1], Colors.highlight[2], Colors.highlight[3])
+							objectivesCompleted = objectivesCompleted + 1
+						else
+							watchText:SetTextColor(Colors.offwhite[1], Colors.offwhite[2], Colors.offwhite[3])
+						end
+
+						watchText:ClearAllPoints()
+						watchText:SetPoint("TOPRIGHT", "QuestWatchLine"..(watchTextIndex - 1), "BOTTOMRIGHT", 0, -4)
+
+						--watchText:Show()
+
+						watchTextIndex = watchTextIndex + 1
+					end
+
+					-- Brighten the quest title if all the quest objectives were met
+					watchText = _G["QuestWatchLine"..(watchTextIndex - numObjectives - 1)]
+					if ( objectivesCompleted == numObjectives ) then
+						watchText:SetTextColor(Colors.title[1], Colors.title[2], Colors.title[3])
+					else
+						watchText:SetTextColor(Colors.title[1]*.75, Colors.title[2]*.75, Colors.title[3]*.75)
+					end
+
+				end
+			end
+		end
+
+		local top, bottom
+
+		local lineID = 1
+		local line = _G["QuestWatchLine"..lineID]
+		top = line:GetTop()
+
+		while line do
+			if (line:IsShown()) then
+				line:SetShadowOffset(0,0)
+				line:SetShadowColor(0,0,0,0)
+				line:SetFontObject(line.isTitle and config.FontObjectTitle or config.FontObject)
+				local _,size = line:GetFont()
+				local spacing = size*.2 - size*.2%1
+
+				line:SetJustifyH("RIGHT")
+				line:SetJustifyV("BOTTOM")
+				line:SetIndentedWordWrap(false)
+				line:SetWordWrap(true)
+				line:SetNonSpaceWrap(false)
+				line:SetSpacing(spacing)
+
+				dummyLine:SetFontObject(line:GetFontObject())
+				dummyLine:SetText(line:GetText() or "")
+				dummyLine:SetSpacing(spacing)
+
+				line:SetWidth(config.TrackerWidth)
+				line:SetHeight(dummyLine:GetHeight())
+
+				bottom = line:GetBottom()
+			end
+
+			lineID = lineID + 1
+			line = _G["QuestWatchLine"..lineID]
+		end
+
+		-- Avoid a nil bug that sometimes can happen with no objectives tracked,
+		-- in weird circumstances I have been unable to reproduce.
+		if (top and bottom) then
+			tracker:SetHeight(top - bottom)
+		end
+
+	end)
+
+end
+
+Tracker.InitializeMovableFrameAnchor = function(self)
+
+	local anchor = MFM:RequestAnchor()
+	anchor:SetTitle(TRACKER_HEADER_OBJECTIVE)
+	anchor:SetScalable(false)
+	--anchor:SetMinMaxScale(.75, 1.25, .05)
+	anchor:SetSize(config.Size[1], config.TrackerHeight)
+	anchor:SetPoint(unpack(defaults.profile.savedPosition.Azerite))
+	anchor:SetScale(defaults.profile.savedPosition.Azerite.scale)
+	anchor.frameOffsetX = 0
+	anchor.frameOffsetY = 0
+	anchor.framePoint = "BOTTOM"
+	anchor.Callback = function(anchor, ...) self:OnAnchorUpdate(...) end
+
+	self.anchor = anchor
+
+end
+
+Tracker.UpdateTrackerPosition = function(self)
+	if (not self.frame) then return end
+
+	QuestWatchFrame:SetParent(self.frame)
+	QuestWatchFrame:SetWidth(config.TrackerWidth)
+	QuestWatchFrame:SetHeight(math_min(UIParent:GetHeight() - config.BottomOffset - config.TopOffset, config.TrackerHeight))
+	QuestWatchFrame:SetClampedToScreen(false)
+	QuestWatchFrame:SetAlpha(.9)
+	QuestWatchFrame:ClearAllPoints()
+	QuestWatchFrame:SetPoint("BOTTOMRIGHT", self.frame, "BOTTOMRIGHT", 0, 0)
+end
+
+Tracker.UpdatePositionAndScale = function(self)
+
+	local savedPosition = self.currentLayout and self.db.profile.savedPosition[self.currentLayout]
+	if (savedPosition) then
+		local point, x, y = unpack(savedPosition)
+		local scale = savedPosition.scale
+		local frame = self.frame
+		local anchor = self.anchor
+
+		-- Set the scale before positioning,
+		-- or everything will be wonky.
+		frame:SetScale(scale * ns.API.GetDefaultElementScale())
+
+		if (anchor and anchor.framePoint) then
+			-- Position the frame at the anchor,
+			-- with the given point and offsets.
+			frame:ClearAllPoints()
+			frame:SetPoint(anchor.framePoint, anchor, anchor.framePoint, (anchor.frameOffsetX or 0)/scale, (anchor.frameOffsetY or 0)/scale)
+
+			-- Parse where this actually is relative to UIParent
+			local point, x, y = ns.API.GetPosition(frame)
+
+			-- Reposition the frame relative to UIParent,
+			-- to avoid it being hooked to our anchor in combat.
+			frame:ClearAllPoints()
+			frame:SetPoint(point, UIParent, point, x, y)
+		end
+	end
+
+end
+
+Tracker.OnAnchorUpdate = function(self, reason, layoutName, ...)
+	local savedPosition = self.db.profile.savedPosition
+	local lockdown = InCombatLockdown()
+
+	if (reason == "LayoutDeleted") then
+		if (savedPosition[layoutName]) then
+			savedPosition[layoutName] = nil
+		end
+
+	elseif (reason == "LayoutsUpdated") then
+
+		if (savedPosition[layoutName]) then
+
+			self.anchor:SetScale(savedPosition[layoutName].scale or self.anchor:GetScale())
+			self.anchor:ClearAllPoints()
+			self.anchor:SetPoint(unpack(savedPosition[layoutName]))
+
+			local defaultPosition = defaults.profile.savedPosition[layoutName]
+			if (defaultPosition) then
+				self.anchor:SetDefaultPosition(unpack(defaultPosition))
+			end
+
+			self.initialPositionSet = true
+				--self.currentLayout = layoutName
+
+		else
+			-- The user is unlikely to have a preset with our name
+			-- on the first time logging in.
+			if (not self.initialPositionSet) then
+				--print("setting default position for", layoutName, self.frame:GetName())
+
+				local defaultPosition = defaults.profile.savedPosition.Azerite
+
+				self.anchor:SetScale(defaultPosition.scale)
+				self.anchor:ClearAllPoints()
+				self.anchor:SetPoint(unpack(defaultPosition))
+				self.anchor:SetDefaultPosition(unpack(defaultPosition))
+
+				self.initialPositionSet = true
+				--self.currentLayout = layoutName
+			end
+
+			savedPosition[layoutName] = { self.anchor:GetPosition() }
+			savedPosition[layoutName].scale = self.anchor:GetScale()
+		end
+
+		self.currentLayout = layoutName
+
+		self:UpdatePositionAndScale()
+
+	elseif (reason == "PositionUpdated") then
+		-- Fires when position has been changed.
+		local point, x, y = ...
+
+		savedPosition[layoutName] = { point, x, y }
+		savedPosition[layoutName].scale = self.anchor:GetScale()
+
+		self:UpdatePositionAndScale()
+
+	elseif (reason == "ScaleUpdated") then
+		-- Fires when scale has been mousewheel updated.
+		local scale = ...
+
+		savedPosition[layoutName].scale = scale
+
+		self:UpdatePositionAndScale()
+
+	elseif (reason == "Dragging") then
+		-- Fires on every drag update. Spammy.
+		--if (not self.incombat) then
+			self:OnAnchorUpdate("PositionUpdated", layoutName, ...)
+		--end
+
+	elseif (reason == "CombatStart") then
+		-- Fires right before combat lockdown for visible anchors.
+
+
+	elseif (reason == "CombatEnd") then
+		-- Fires when combat lockdown ends for visible anchors.
+
+	end
+end
+
+Tracker.OnEvent = function(self, event, ...)
+	if (event == "PLAYER_ENTERING_WORLD") then
+		QuestWatchFrame:SetAlpha(.9)
+		if (self.queueImmersionHook) then
+			local frame = ImmersionFrame
+			if (frame) then
+				self.queueImmersionHook = nil
+				frame:HookScript("OnShow", function() QuestWatchFrame:SetAlpha(0) end)
+				frame:HookScript("OnHide", function() QuestWatchFrame:SetAlpha(.9) end)
+			end
+		end
+	end
+	self:UpdateTrackerPosition()
+end
+
+Tracker.OnInitialize = function(self)
+	self.db = ns.db:RegisterNamespace("Tracker", defaults)
+
+	self:SetEnabledState(self.db.profile.enabled)
+
+	-- Register the available layout names
+	-- with the movable frames manager.
+	if (MFM) then
+		MFM:RegisterPresets(self.db.profile.savedPosition)
+	end
+
+	self:InitializeTracker()
+	self:InitializeMovableFrameAnchor()
+
+	self.queueImmersionHook = IsAddOnEnabled("Immersion")
+	self:RegisterEvent("VARIABLES_LOADED", "OnEvent")
+	self:RegisterEvent("PLAYER_ENTERING_WORLD", "OnEvent")
+end
