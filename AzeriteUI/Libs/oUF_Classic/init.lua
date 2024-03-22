327662900b9ccd079b6b46330d7fc86b3cf20e0c
Lars Norberg <github@larsnorbergofficial.com>
2024-03-22 10:46:34 +0100

    cata constants

--- AzeriteUI/Libs/oUF_Classic/init.lua
+++ AzeriteUI/Libs/oUF_Classic/init.lua
@@ -2,8 +2,12 @@ local _, ns = ...
 ns.oUF = {}
 ns.oUF.Private = {}
 
+local version = select(4, GetBuildInfo())
+
 ns.oUF.isRetail = (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE)
 ns.oUF.isClassic = (WOW_PROJECT_ID == WOW_PROJECT_CLASSIC)
 ns.oUF.isTBC = (WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC)
 ns.oUF.isWrath = (WOW_PROJECT_ID == WOW_PROJECT_WRATH_CLASSIC)
-ns.oUF.WoW10 = select(4, GetBuildInfo()) >= 100000
+--ns.oUF.isCata = WOW_PROJECT_ID == (WOW_PROJECT_CATA_CLASSIC or 99) -- NYI in first build
+ns.oUF.isCata = (version >= 40400) and (version < 50000)
+ns.oUF.WoW10 = version >= 100000
